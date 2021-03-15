7b5cf039c0289c0942b915b0632a831701a3a68f
hckaraman <cagrkaraman@gmail.com>
2021-03-15 22:09:14 +0300

    content update

--- src/app.R
+++ src/app.R
@@ -4,71 +4,30 @@ library(tidyverse)
 library(vroom)
 library(shinycssloaders)
 library(highcharter)
-library(RSQLite)
 library(stringr)
 library(xts)
 
 options(digits = 2)
 
 
-ui <-fluidPage(
-  tags$head(
-    tags$script(src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.16/iframeResizer.contentWindow.min.js",
-                type="text/javascript")
-  ),
-  
-  # Header
-  headerPanel(
-    title=tags$a(href='http://www.hidrosaf.com',tags$img(src='https://www.metu.edu.tr/system/files/logo_orj/5/5.4.jpg', height = 125, width = 100*2.85*1.75), target="_blank"),
-    tags$head(tags$link(rel = "icon", type = "image/png", href = "dwq_logo_small.png"), windowTitle="Lake profile dashboard")
-  ),
-  
-  #,
-  
-  # Input widgets
-  fluidRow(
-    column(7,
-           tabsetPanel(id="ui_tab",
-                       tabPanel("Map",
-                                column(12,shinycssloaders::withSpinner(leaflet::leafletOutput("map", height="600px"),size=2, color="#0080b7"))
-                       )
-           ),
-    ),
-    column(5,tabsetPanel(id="plot_tabs",
-                         
-                         tabPanel("Plot",
-                                  fluidRow(column(12,h4("Remotely sensed ponds"),
-                                                  withSpinner(
-                                                    highchartOutput(outputId = "plot", height = "600px")
-                                                  ),))),
-                         tabPanel("Table",
-                                  fluidRow(column(12,
-                                                  withSpinner(
-                                                    DT::dataTableOutput(outputId = "table", height = "600px")
-                                                  ),)))
-                         
-                         
-    ))))
+
 
 server <- function(input, output, session){
   
   
   reactive_objects=reactiveValues()
   reactive_objects$Station <- '03-18'
+  load("data/data.RData")
   
-  
-  data_file <- './data/Data.db'
-  conn <- dbConnect(RSQLite::SQLite(), data_file)
   station_file <- './data/stations.geojson'
   river_file <- './data/river.geojson'
-  query = "SELECT * FROM Discharge d where d.Station = '03-18';"
-  data = dbGetQuery(conn, query)
-  data <- rapply(object = data, f = round, classes = "numeric", how = "replace", digits = 6) 
+
+
+  df <- data[which(data$Station == "1201"),]
+  df <- rapply(object = df, f = round, classes = "numeric", how = "replace", digits = 6) 
   
   stations <-  rgdal::readOGR(station_file)
   rivers <-  rgdal::readOGR(river_file)
-  mypalette <- colorNumeric( palette="viridis", domain=pond_polygon$id, na.color="transparent")
-  
   
   map = leaflet::createLeafletMap(session, 'map')
   
@@ -117,12 +76,11 @@ server <- function(input, output, session){
     output$plot <- renderHighchart({
       
       station <- reactive_objects$Station
-      query = str_interp("SELECT * FROM Discharge d where d.Station = '${station}';")
-      # query = "SELECT * FROM Discharge d"
-      data = dbGetQuery(conn, query)
-      data$Dischage <- as.numeric(data$Dischage)
-      data <- rapply(object = data, f = round, classes = "numeric", how = "replace", digits = 6) 
-      dfx = xts(data$Dischage, order.by=as.Date(data$Date))
+      df <- data[which(data$Station == station),]
+      
+      df$Dischage <- as.numeric(df$Dischage)
+      df <- rapply(object = df, f = round, classes = "numeric", how = "replace", digits = 6) 
+      dfx = xts(df$Dischage, order.by=as.Date(df$Date))
       
       highchart(type = "stock") %>% 
         hc_title(text = paste("Observed discharge at station : ",station)) %>%
@@ -143,12 +101,11 @@ server <- function(input, output, session){
     output$table <- DT::renderDataTable({
       
       station <- reactive_objects$Station
-      query = str_interp("SELECT * FROM Discharge d where d.Station = '${station}';")
-      # query = "SELECT * FROM Discharge d"
-      data = dbGetQuery(conn, query)
-      data$Dischage <- as.numeric(data$Dischage)
-      data <- select(data,"Date","Dischage")
-      DT::datatable(data,  extensions = 'Buttons',options = list(dom = 'Blfrtip',
+      df <- data[which(data$Station == station),]
+      df$Dischage <- as.numeric(df$Dischage)
+      df <- select(df,"Date","Dischage")
+      row.names(df) <- NULL
+      DT::datatable(df,  extensions = 'Buttons',options = list(dom = 'Blfrtip',
                                           buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
                                           lengthMenu = list(c(10,25,50,-1),
                                                             c(10,25,50,"All"))))
